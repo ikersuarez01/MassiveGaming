f94ccaa1f5797160635309fad8698c5f249c0ff2
luciamol <58294628+luciamol@users.noreply.github.com>
2023-02-02 11:49:38 +0100

    ProyectoBase

---
+++ MassiveGaming/src/main/java/com/IkerLucia/MassiveGaming/MassiveGamingApplication.java
@@ -0,0 +1,13 @@
+package com.IkerLucia.MassiveGaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MassiveGamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MassiveGamingApplication.class, args);
+	}
+
+}

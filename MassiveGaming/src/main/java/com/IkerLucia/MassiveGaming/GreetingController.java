7bbbceef15a2be71d580ae5760a1e7a8555587af
ikersuarez01 <ikersuarez01@gmail.com>
2023-02-11 12:10:43 +0100

    Project

--- MassiveGaming/src/main/java/com/IkerLucia/MassiveGaming/GreetingController.java
+++ MassiveGaming/src/main/java/com/IkerLucia/MassiveGaming/GreetingController.java
@@ -1,11 +1,41 @@
 package com.IkerLucia.MassiveGaming;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
+	
+	private String infoCompartida;
+
+	@GetMapping("/formulario")
+	public String formulario(Model model) {
+
+		return "formulario";
+	}
+	@PostMapping("/resultadoFormulario")
+	public String resultadoFormulario(@RequestParam String info, HttpSession sesion) {
+
+		sesion.setAttribute("infoUsuario", info);
+		infoCompartida = info;
+
+		return "resultadoFormulario";
+	}
+	@GetMapping("/mostrarDatos")
+	public String mostrarDatos(Model model, HttpSession sesion) {
+
+		String infoUsuario = (String) sesion.getAttribute("infoUsuario");
+
+		model.addAttribute("infoUsuario", infoUsuario);
+		model.addAttribute("infoCompartida", infoCompartida);
+
+		return "datos";
+	}
 
 	@GetMapping("/greeting")
 	public String greeting(Model model) {
@@ -14,5 +44,4 @@ public class GreetingController {
 
 		return "greeting_template";
 	}
-
 }

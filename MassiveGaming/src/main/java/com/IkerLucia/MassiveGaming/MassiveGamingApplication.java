b6ef83644b630935b1e7f1283c9ba80f8d13500e
ikersuarez01 <ikersuarez01@gmail.com>
2023-04-22 12:07:22 +0200

    cache

--- MassiveGaming/src/main/java/com/IkerLucia/MassiveGaming/MassiveGamingApplication.java
+++ MassiveGaming/src/main/java/com/IkerLucia/MassiveGaming/MassiveGamingApplication.java
@@ -1,8 +1,12 @@
 package com.IkerLucia.MassiveGaming;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -13,9 +17,17 @@ public class MassiveGamingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MassiveGamingApplication.class, args);
 	}
-
+	
+	private static final Log LOG = LogFactory.getLog(MassiveGamingApplication.class);
+	
 	@Bean
 	public Queue myQueue() {
     	return new Queue("mensajes", false);
+	}
+	
+	@Bean
+	public CacheManager cacheManager() {
+		LOG.info("Activating cache...");
+    	return new ConcurrentMapCacheManager("anuncios");
 	}
 }
--- MassiveGaming/src/main/java/com/IkerLucia/MassiveGaming/repository/VideojuegoRepository.java
+++ MassiveGaming/src/main/java/com/IkerLucia/MassiveGaming/repository/VideojuegoRepository.java
@@ -2,10 +2,19 @@ package com.IkerLucia.MassiveGaming.repository;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.IkerLucia.MassiveGaming.model.*;
 
+@CacheConfig(cacheNames="videojuegos")
 public interface VideojuegoRepository extends JpaRepository<Videojuego, Long> {
+	 @Cacheable
 	 List<Videojuego> findByNombre(String nombre);
+	 @CacheEvict(allEntries=true)
+	 Videojuego save(Videojuego videojuego);
+	 @Cacheable
+	 List<Videojuego> findAll();
 }

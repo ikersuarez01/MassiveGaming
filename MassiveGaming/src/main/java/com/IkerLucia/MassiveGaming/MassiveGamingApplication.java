eea813f14af6e693659da15a4b76f2f3f916b067
luciamol <l.molinero.2019@alumnos.urjc.es>
2023-04-27 13:30:16 +0200

    IPConfig

--- MassiveGaming/src/main/java/com/IkerLucia/MassiveGaming/MassiveGamingApplication.java
+++ MassiveGaming/src/main/java/com/IkerLucia/MassiveGaming/MassiveGamingApplication.java
@@ -1,5 +1,7 @@
 package com.IkerLucia.MassiveGaming;
 
+import java.util.Arrays;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.amqp.core.Queue;
@@ -11,9 +13,15 @@ import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
+
+
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
+@EnableHazelcastHttpSession
 public class MassiveGamingApplication {
 	
 
@@ -32,5 +40,17 @@ public class MassiveGamingApplication {
 	public CacheManager cacheManager() {
 		LOG.info("Activating cache...");
     	return new ConcurrentMapCacheManager("videojuegos", "consolas");
+	}
+	@Bean
+	public Config config() {
+
+	    Config config = new Config();
+
+	    JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+
+	    joinConfig.getMulticastConfig().setEnabled(false);
+	    joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Arrays.asList("massivegaming-web-1", "massivegaming-web-2"));
+
+	    return config;
 	}
 }
